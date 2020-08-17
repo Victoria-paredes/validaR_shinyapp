96f47b32dba2b3ece48da15b078de25ee79a239e
Crparedes <craparedesca@unal.edu.co>
2020-08-17 18:49:55 -0500

    new directories organization

---
+++ Modules/Bibliogr.R
@@ -0,0 +1,19 @@
+bibliogrModuleUI <- function(id) {
+  box(width = 12,
+    h5(HTML('
+<table>
+  <tr><td>&zwnj;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+          &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</td></tr>
+
+  <tr valign="top">
+    <td align="right">[NAME et al., ]&emsp;</td>
+    <td> DETAILS <a href="https://doi.org/">https://doi.org/</a></td>
+  </tr><tr><td>&zwnj;</td></tr>
+
+  <tr valign="top">
+    <td align="right">[R Core Team, 2020]&emsp;</td>
+    <td>R Core Team, (2020). R: A language and environment for statistical computing. R Foundation for Statistical Computing,
+    Vienna, Austria. <a href="https://www.R-project.org/">https://www.R-project.org/</a></td>
+  </tr>tr><td>&zwnj;</td></tr>
+</table>')))
+}
