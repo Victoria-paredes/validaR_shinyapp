1a9267d6b664f5411a034f0cadd68a817d10acda
Crparedes <craparedesca@unal.edu.co>
2021-01-08 21:22:10 -0500

    minor improvements in definitions module

--- Modules/mdlDefiniciones.R
+++ Modules/mdlDefiniciones.R
@@ -2,8 +2,10 @@
 
 definicionesUI <- function(id) {
   ns <- NS(id)
-  fluidRow(column(2, pickerInput(ns("trm"), label = "Buscar término", choices = nomConceptos, selected = 0,
-                                 options = list(`live-search` = TRUE))),
+  fluidRow(column(2, pickerInput(ns("trm"), label = "Buscar término", choices = nomConceptos, selected = 0, multiple = TRUE, 
+                                 options = list(`live-search` = TRUE,
+                                                `none-selected-text` = 'Escriba una opción...',
+                                                `none-results-text` = 'Sin resultados...'))),
            column(10, uiOutput(ns('definicion'))))
 }
 
